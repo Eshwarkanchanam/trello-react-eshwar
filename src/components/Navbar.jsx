852b6b955d3a49c3ee581cd5685809dac8e93a2f
Eshwar Kanchanam <eshwar.kanchanam.29.1@mountblue.tech>
2024-08-29 17:38:23 +0530

    refactored navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,11 +18,11 @@ const Navbar = () => {
         let board = response.data;
         let boardId = board.id;
         setBoardName("");
-        navigate(`/boards/${boardId}`);
         enqueueSnackbar("created board successfully", {
           variant: "success",
           autoHideDuration: 3000,
         });
+        navigate(`/boards/${boardId}`);
       } else {
         throw new Error("something went wrong, please try again later");
       }
@@ -38,8 +38,14 @@ const Navbar = () => {
   return (
     <AppBar sx={{ p: 2, bgcolor: "#1D2125", position: "static" }}>
       <Box sx={{ display: "flex" }}>
-        <Stack variant="h5" direction={'row'} alignItems={'center'} fontSize={20}>
-          <FaTrello style={{marginRight:6}}/> <Typography fontSize={20}>Trello</Typography>
+        <Stack
+          variant="h5"
+          direction={"row"}
+          alignItems={"center"}
+          fontSize={20}
+        >
+          <FaTrello style={{ marginRight: 6 }} />{" "}
+          <Typography fontSize={20}>Trello</Typography>
         </Stack>
 
         <CreateComponent
